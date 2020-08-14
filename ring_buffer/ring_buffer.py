7a62781abc4847d6e167ee47ad797d9e322aaf5d
Gabriel Romero <gabester78@gmail.com>
2020-08-14 11:42:38 -0500

    Ringbuffer done.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
+from collections import deque
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.index = 0
+        self.queue = deque()
 
     def append(self, item):
-        pass
+        if len(self.queue) == self.capacity:
+            self.queue[self.index] = item
+        else:
+            self.queue.append(item)
+        self.index = (self.index + 1) % self.capacity
 
     def get(self):
-        pass
+        the_one_ring = []
+        for i in self.queue:
+            the_one_ring.append(i)
+        return the_one_ring
